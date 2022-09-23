e1f206e52a1c4fa8f35b8f29d64ca5f9db38fa4b
NaartjieBoer <danielvandervyverw@gmail.com>
2022-09-23 11:50:01 +0200

    Player movement - complete
    Complete player movement script

--- Galaxy Shooter/Assets/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -8,15 +8,19 @@ public class Player : MonoBehaviour
     //player speed variable
     private float _speed = 3.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // take the current position = new position (0,0,0)
+        // Start position
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
+    {
+        PlayerMovement();
+    }
+
+    public void PlayerMovement()
     {
         //input from user
         float horizontalInput = Input.GetAxis("Horizontal");
@@ -24,5 +28,17 @@ public class Player : MonoBehaviour
 
         //movement(left, right, up, down)
         transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * _speed * Time.deltaTime);
+
+        //restraints
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -.38f, 0), 0);
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
